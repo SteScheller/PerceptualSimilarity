5798086d4dc3f2b3c46448bb09c0f716efeefeeb
Stefan Scheller <stefan.scheller@visus.uni-stuttgart.de>
2020-01-21 15:38:15 +0100

    - basic image loading to separate function with caching - cleaned up file extension handling in image loading

--- util/util.py
+++ util/util.py
@@ -6,22 +6,44 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import torch
+import functools
 
-# Note:
-# This method was modified such that only a part of the image can be loaded.
-#
-#   partOrigin:     tuple of x and y indices
-#   partDim:        tuple with width and height of image part
-def load_image(path, partOrigin=None, partDim=(0, 0)):
-    if(path[-3:] == 'dng'):
+
+#------------------------------------------------------------------------------
+# Global Settings
+#------------------------------------------------------------------------------
+IMG_CACHE_COUNT = 1000
+
+#------------------------------------------------------------------------------
+# Functions
+#------------------------------------------------------------------------------
+"""
+Sub-function for loading uncropped images with caching
+"""
+@functools.lru_cache(maxsize=IMG_CACHE_COUNT)
+def load_image_cached(path):
+    extension = os.path.splitext(path)[1]
+
+    if(extension == '.dng'):
         import rawpy
         with rawpy.imread(path) as raw:
             img = raw.postprocess()
-    elif(path[-3:]=='bmp' or path[-3:]=='jpg' or path[-3:]=='png'):
+    elif(extension=='.bmp' or extension=='.jpg' or extension=='png'):
         import cv2
         img = cv2.imread(path)[:,:,::-1]
     else:
-        img = (255*plt.imread(path)[:,:,:3]).astype('uint8')
+        img = (255 * plt.imread(path)[:,:,:3]).astype('uint8')
+
+    return img
+
+"""
+Loads an image (or a part of it) from the given path
+
+    partOrigin:     tuple of x and y indices
+    partDim:        tuple with width and height of image part
+"""
+def load_image(path, partOrigin=None, partDim=(0, 0)):
+    img = load_image_cached(path)
 
     if partOrigin is not None:
         x, y = partOrigin
@@ -56,3 +78,4 @@ def im2tensor(image, imtype=np.uint8, cent=1., factor=255./2.):
 # def im2tensor(image, imtype=np.uint8, cent=1., factor=1.):
     return torch.Tensor((image / factor - cent)
                         [:, :, :, np.newaxis].transpose((3, 2, 0, 1)))
+
